c5f35361fafd944da46d916cabeb6ec7cac5e1dc
FelipeMoleiro <felipemoleiro@hotmail.com>
2021-06-23 08:57:46 -0300

    Demonstration Of working algorithm with 2 images on jupyter notebook

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # SeamCarving
-Seam Carving Image Processing
+Content-Aware Resizing using Seam Carving Image Processing
+
+
+# Objetivo Principal
+
+O objetivo principal desse projeto é implementar o algoritmo de Seam Carving para fazer Resize de imagens levando em conta o conteudo da imagem. O que queremos é aumentar e diminuir o tamanho de uma imagem levando em conta o conteudo da imagem, ou seja, queremos inserir ou remover linhas e colunas sem distorcer o conteudo principal da imagem.
+
+Essa é uma tecnica muito interessante que permite uma imagem se adequar a diferentes formatos sem distorcer a imagem. Uma utilidade citada pelo Paper original diz que uma ideia interessante é utilizar esse algoritmo para imagens em site da web que devem ser visualizadas em dispositivos diferentes que tem aspect ratios diferentes.
+
+Outra utilidade desse algoritmo discutido no Paper original tambem é a capacidade de remover objetos da
+
+# input Images
+
+Qualquer imagem pode ser input para esse algoritmo, entretanto as imagens que se darão melhor são as imagens que são compostas majoritariamente por background e com poucos elementos importantes na imagem.
+
+As imagens utilizadas nessa primeira parte do projeto foram obtidas do exemplo do algoritmo existente na wikipedia:
+    Site: https://en.wikipedia.org/wiki/Seam_carving 
+E tambem a imagem do quadro "A persistencia da Memoria" do pintor Salvador Dali que foi usada de exemplo em video explicando o algoritmo
+    Video: https://www.youtube.com/watch?v=ALcohd1q3dk&ab_channel=TheJuliaProgrammingLanguage
+    Imagem: https://i.pinimg.com/originals/c6/24/f5/c624f57595292a5ec8a229386be745d6.jpg
+
+As imagens para o projeto final serão imagens interessantes que nosso grupo consiga encontrar. Mas as imagens mais seguras para fazer os testes caso tenhamos dificuldades de encontrar imagens legais, podemos pegar as imagens do Paper original que apresentou esse conceito de Seam Carving.
+    Paper: https://faculty.idc.ac.il/arik/SCWeb/imret/imret.pdf
+
+# Descrição dos Passos
+
--- trabalhoFinal.py
+++ trabalhoFinal.py
@@ -6,22 +6,15 @@ import imageio
 
 #Sobel Img Edge Detection
 def edgeImg(img):
-    #gx = cv2.Sobel(img, cv2.CV_32F, 1, 0, ksize=1)
-    #gy = cv2.Sobel(img, cv2.CV_32F, 1, 0, ksize=1)
-    #mag, angle = cv2.cartToPolar(gx, gy, angleInDegrees=True)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     sobelX = cv2.Sobel(gray,cv2.CV_64F,1,0)
     sobelY = cv2.Sobel(gray,cv2.CV_64F,0,1)
 
-
-    newImg = np.sqrt(np.power(sobelX,2) + np.power(sobelY,2))
-
-    return newImg
+    return np.sqrt(np.power(sobelX,2) + np.power(sobelY,2))
 
 #use dynamic programming to find Min Energy Path
 def calculateMinEnergyPath(energyImg):
     minEnergyPath = np.zeros(energyImg.shape)
-    dir = np.zeros(energyImg.shape)
     n = energyImg.shape[0]
     m = energyImg.shape[1]
 
@@ -29,29 +22,16 @@ def calculateMinEnergyPath(energyImg):
     for j in range(0,m):
         minEnergyPath[n-1][j] = energyImg[n-1][j]
 
-    print(minEnergyPath)
-
     #builds matrix from bottom up
     for i in range(n-2,-1,-1):
         for j in range(0,m):
-            #print(i,j)
-
             #just making sure we dont go out of bounds and finding the min
-            minPos = j
-            center = j
-            min = minEnergyPath[i+1][center]
-
-            if(center>0 and minEnergyPath[i+1][center-1] < min):
-                min = minEnergyPath[i+1][center-1]
-                minPos = center -1
-
-            if(center<m-1 and minEnergyPath[i+1][center+1] < min):
-                min = minEnergyPath[i+1][center+1]
-                minPos = center+1
+            left = max(0,j-1)
+            right = min(m-1,j+1)
             
 
-            minEnergyPath[i][j] = energyImg[i][j] + min
-            #dir[i][j] = minPos - center # -1, 0 ou
+            minEnergyPath[i][j] = energyImg[i][j] + min(minEnergyPath[i+1][left:right+1])
+
     return minEnergyPath
 
 def printMinSeam(img,minEnergyPath):
@@ -68,7 +48,7 @@ def printMinSeam(img,minEnergyPath):
             minVal = minEnergyPath[0][i]
 
     printImg = np.array(img)
-    printImg[0][minPos] = [255,255,255]
+    printImg[0][minPos] = [255,0,0]
     for i in range(1,n):
         #find min from three down
         center = minPos
@@ -83,25 +63,60 @@ def printMinSeam(img,minEnergyPath):
             minPos = center+1
 
 
-        printImg[i][minPos] = [255,255,255]
+        printImg[i][minPos] = [255,0,0]
     
     plt.imshow(printImg)
     plt.show()
     return
 
+def removeSeam(img,minEnergyPath):
+    n = minEnergyPath.shape[0]
+    m = minEnergyPath.shape[1]
+    
+    minPos = 0
+    minVal = minEnergyPath[0][0]
+
+    #find Min Seam
+    for i in range(1,m):
+        if(minEnergyPath[0][i] < minVal):
+            minPos = i
+            minVal = minEnergyPath[0][i]
+
+    newImg = np.zeros((img.shape[0],img.shape[1]-1,img.shape[2]),dtype=np.uint8)
+
+    newImg[0] = np.concatenate((img[0][0:minPos],img[0][minPos+1:img.shape[1]]))
+    for i in range(1,n):
+        #find min from three down
+        center = minPos
+        min = minEnergyPath[i][center]
+
+        if(center>0 and minEnergyPath[i][center-1] < min):
+            min = minEnergyPath[i][center-1]
+            minPos = center -1
+
+        if(center<m-1 and minEnergyPath[i][center+1] < min):
+            min = minEnergyPath[i][center+1]
+            minPos = center+1
+
+        newImg[i] = np.concatenate((img[i][0:minPos],img[i][minPos+1:img.shape[1]]))
+    
+    return newImg
+
+def removeNSeams(img,seamNumber):
+    newImg = img
+    for i in range(0,seamNumber):
+        print ('removing seam ',i+1, ' of ', seamNumber, end="\r")
+        newEP = calculateMinEnergyPath(edgeImg(newImg))
+        newImg = removeSeam(newImg,newEP)
+
+
+    return newImg
+
 #img = imageio.imread('imgs/Broadway_tower_edit.jpg')
-img = imageio.imread('imgs/PersistenceOfMemory.jpg')
+img = imageio.imread('imgs/persistenceLowRes.jpg')
 
-plt.imshow(img)
+
+newImg = removeNSeams(img,10)
+
+plt.imshow(newImg)
 plt.show()
-
-edge = edgeImg(img)
-
-minEnergyPath = calculateMinEnergyPath(edge)
-
-plt.imshow(minEnergyPath,cmap="gray")
-plt.show()
-
-printMinSeam(img,minEnergyPath)
-
-#print(edge.shape)
